68c3d053b5ad8927dd56128683135eea0b904608
Bryan Alejandro Rodriguez Gomez <158237554+BryanAlRG@users.noreply.github.com>
2026-03-09 10:59:46 -0700

    Actividad de evidencia 2_U2 Singleton

---
+++ Actividad de evidencia 2_U2 Singleton/Central_911.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Central_911
+    {
+        private static Central_911 _instance;
+        private static readonly object _lock = new object();
+
+        public string Central {  get; private set; }
+
+        private Central_911() 
+        {
+            Central = "Central 911";
+        }
+
+        public static Central_911 Obtener_Instancia()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Central_911();
+                    }
+                }    
+            }
+            return _instance;
+        }
+
+        public void ConectarLlamada(Operador operador, string tipoEmergencia)
+        {
+            Console.WriteLine("\nLlamada conectada con el operador" + operador.Nombre);
+            operador.AtiendeEmergencia(tipoEmergencia);
+        }
+    }
+}
---
+++ Actividad de evidencia 2_U2 Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Central_911 Llamada1 = Central_911.Obtener_Instancia();
+            Central_911 Llamada2 = Central_911.Obtener_Instancia();
+            Central_911 Llamada3 = Central_911.Obtener_Instancia();
+            Central_911 Llamada4 = Central_911.Obtener_Instancia();
+
+            Operador op1 = new Operador(1, "Laura");
+            Operador op2 = new Operador(2, "Carlos");
+            Operador op3 = new Operador(3, "Arturia");
+            Operador op4 = new Operador(4, "Daniel");
+
+            Llamada1.ConectarLlamada(op1, "Incendio");
+            Llamada2.ConectarLlamada(op2, "Violeta");
+            Llamada3.ConectarLlamada(op4, "Accidente");
+            Llamada4.ConectarLlamada(op3, "Intento de suicidio");
+
+            Console.WriteLine(ReferenceEquals(Llamada1, Llamada2));
+            Console.WriteLine(ReferenceEquals(Llamada1, Llamada3));
+            Console.WriteLine(ReferenceEquals(Llamada1, Llamada4));
+
+            Console.ReadKey();
+        }
+    }
+}
